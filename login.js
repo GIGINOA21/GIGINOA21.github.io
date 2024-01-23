1c769255912b235dec3628f8316f2243bd3df026
Jireh Gonzalez Aguilar <156270750+GIGINOA21@users.noreply.github.com>
2024-01-23 09:38:22 -0600

    Create login.js

---
+++ login.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded" , function(){
+   console.log("Hello, World!");
+    var inputEmail = document.getElementById(´email´);
+    var inputPassword = document .getElementById(´password
+    console.log(inputPassword);
+}); 
